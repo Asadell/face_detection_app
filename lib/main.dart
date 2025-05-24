e202a3ba182bfaeee362d365353f417f4061dd2a
Asadell <m.satrioasadell@gmail.com>
2025-05-24 23:09:39 +0700

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
+import 'package:face_detection_app/ui/camera_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const CameraPage(),
     );
   }
 }
